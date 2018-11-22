8072f5537c9c23909bd717f6d8bf6f79d905772a
Craig Dennis <zephni@hotmail.co.uk>
2018-11-22 18:29:51 +0000

    Update to latest Unity, TextMesh added, Cinemachine added

---
+++ Assets/MugHeadStudios/2D/CharacterController/CharacterInput2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MugHeadStudios
+{
+	[RequireComponent(typeof(CharacterController2D))]
+	public class CharacterInput2D : MugHeadMonoBehaviour
+	{
+		float HorizontalMove;
+		bool Jump = false;
+
+		void FixedUpdate()
+		{
+			UseComponent<CharacterController2D>().Move(HorizontalMove * Time.deltaTime, false, Jump);
+
+			if (Jump)
+				Jump = false;
+		}
+
+		void Update()
+		{
+			if (Input.GetButtonDown("Fire1"))
+			Jump = true;
+
+			HorizontalMove = Input.GetAxis("Horizontal");
+		}
+	}
+}
